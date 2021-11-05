b9b11ab19de64106c21e585425b53406f530c423
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2021-11-04 21:48:37 -0400

    Simple http request to get local IP address

---
+++ iss.js
@@ -0,0 +1,30 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (err, res, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (res.statusCode !== 200) {
+      const msg = `Status Code ${res.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    callback(null, JSON.parse(body).ip);
+  });
+};
+
+
+module.exports = { fetchMyIP };
